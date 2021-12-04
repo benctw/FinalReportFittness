453e81ae34e90099b1381109cc05bf6fdd35257a
Yunhans <91072133+Yunhans@users.noreply.github.com>
2021-12-04 14:57:01 +0800

    webpage 2021/12/4

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # import flask related
-from flask import Flask, request, abort, url_for
+from flask import Flask, request, abort, url_for, render_template
 from urllib.parse import parse_qsl, parse_qs
 import random
 from linebot.models import events
@@ -9,6 +9,21 @@ from service_actions.food import *
 
 # create flask server
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000, debug=True)
 
 @app.route("/callback", methods=['POST'])
 def callback():

2b59532d4fb9aa0dd2b89f5c2e3f4852fed72319
LanningYang <lanning1115@gmail.com>
2021-12-14 15:39:04 +0800

    update BMI.py

--- service_actions/BMI.py
+++ service_actions/BMI.py
@@ -19,18 +19,18 @@ def calculate_BMI(event,recrive_text):
         BMI = weight/(height/100)**2
 
         message.append(TextSendMessage(text='你的BMI為{:.1f}'.format(BMI)))
-        
+
+        if BMI >= 24:
+            message.append(TextMessage(text='你的體重過重了，建議多多運動'))
+            message.append(StickerSendMessage(package_id=6362, sticker_id=11087933))
+        elif 18.5 <= BMI <24:
+            message.append(TextSendMessage(text='你的BMI界於正常範圍，繼續保持'))
+            message.append(StickerSendMessage(package_id=11537, sticker_id=52002734))
+        else:
+            message.append(TextSendMessage(text='你的體重過輕了，建議多吃一點'))
+            message.append(StickerSendMessage(package_id=1070, sticker_id=17847))
+            
     except:
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text = "小幫手目前沒有這個功能哦~"))
-    
-    if BMI >= 24:
-        message.append(TextMessage(text='你的體重過重了，建議多多運動'))
-        message.append(StickerSendMessage(package_id=6362, sticker_id=11087933))
-    elif 18.5 <= BMI <24:
-        message.append(TextSendMessage(text='你的BMI界於正常範圍，繼續保持'))
-        message.append(StickerSendMessage(package_id=11537, sticker_id=52002734))
-    else:
-        message.append(TextSendMessage(text='你的體重過輕了，建議多吃一點'))
-        message.append(StickerSendMessage(package_id=1070, sticker_id=17847))
 
     line_bot_api.reply_message(event.reply_token,message)

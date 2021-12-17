479cfd7fa36eb540f3dd1626666148f84194a4ca
idler <s0973913299@gmailcom>
2021-12-17 10:21:31 +0800

    UD

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def handle_something(event):
             call_service(event)
         elif '我想知道跑步機怎麼用' in recrive_text:
             messages=[]
-            messages.append(ImageSendMessage(original_content_url='https://i.imgur.com/H8O5GVT.png', preview_image_url='https://i.imgur.com/JM2MHSi.png'))
+            messages.append(ImageSendMessage(original_content_url='https://youso.hk/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/g/6/g6481_pioneerdual.jpg', preview_image_url='https://i.imgur.com/JM2MHSi.png'))
             messages.append(TextSendMessage(text='跑步機介紹待定'))
             messages.append(another_service_or_not)
             line_bot_api.reply_message(event.reply_token, messages)  

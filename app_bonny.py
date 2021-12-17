b93420a2d473183ee576c7dc0f92e7717f023691
Bonny <bonnywu2003@gmail.com>
2021-12-17 21:07:40 +0800

    revise food

--- app_bonny.py
+++ app_bonny.py
@@ -1,32 +1,17 @@
 # import flask related
-import re
-from flask import Flask, request, abort, url_for, render_template
+from flask import Flask, request, abort, url_for
 from urllib.parse import parse_qsl, parse_qs
 import random
 from linebot.models import events
 from line_chatbot_api import *
-from service_actions.BMI import *
-from service_actions.introduction import *
 from service_actions.food_bonny import *
+from service_actions.food import *
 from service_actions.allfoodlist import *
+from service_actions.service_origin import *
 
 # create flask server
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def page_not_found(e):
-    return render_template('500.html'), 500
-
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -47,7 +32,7 @@ def callback():
 
 # handle msg
 import os
-import speech_recognition as sr
+#import speech_recognition as sr
 
 def transcribe(wav_path):
     '''
@@ -66,6 +51,9 @@ def transcribe(wav_path):
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
     return None
 
+
+
+
 @handler.add(PostbackEvent)
 def handle_postback(event):
     user_id = event.source.user_id
@@ -74,7 +62,11 @@ def handle_postback(event):
     postback_data = dict(parse_qsl(event.postback.data))
     # print(postback_data.get('action', ''))
     # print(postback_data.get('item', ''))
+    ##########################################################################################
     sticker_list=[(1070, 17839), (6362, 11087920), (11537, 52002734), (8525, 16581293),(8522,16581266),(446,1997),(6362,11087932)]
+
+
+    
     if postback_data.get('action')=='想來點推薦的料理':
         if postback_data.get('item')=='0-300':
             random_number = random.randint(0,len(food_diet1)-1)
@@ -150,7 +142,25 @@ def handle_postback(event):
             messages.append(TextSendMessage(text=f'吃{food_fruit[random_number]}吧，剛好是一份的量!'))
             messages.append(another_or_not)
             line_bot_api.reply_message(event.reply_token, messages)
-            
+
+        
+    elif postback_data.get('action')=='索取備品':
+        sticker_random=sticker_list[random.randint(0,len(sticker_list)-1)]
+        messages=[]
+        messages.append(StickerSendMessage(package_id=sticker_random[0], sticker_id=sticker_random[1]))
+        messages.append(TextSendMessage(text=f'{user_name}, 好的沒問題, 櫃台服務人員將盡快幫您準備{postback_data.get("item", "")}'))
+        messages.append(service_or_not)
+        line_bot_api.reply_message(event.reply_token, messages)
+        
+
+
+
+
+
+
+
+    elif postback_data.get('action')=='food':
+        show_food(event, json.loads(postback_data.get('item', ''))) 
     elif postback_data.get('action')=='還需要':
         all_service(event)  
     elif postback_data.get('action')=='再來一個吧':
@@ -162,46 +172,29 @@ def handle_postback(event):
         line_bot_api.reply_message(event.reply_token, messages)
 
 
-######################以下兩個未知#################################
-    elif postback_data.get('action')=='還需要其他介紹':
-        call_service(event)  
-    elif postback_data.get('action')=='暫時先不用其他介紹':
-        messages=[]
-        messages.append(StickerSendMessage(package_id=11537, sticker_id=52002734))
-        messages.append(TextSendMessage(text='祝您有愉快的健身體驗'))
-        line_bot_api.reply_message(event.reply_token, messages)
-#####################################################################
 
 
+
+
+
+        
 @handler.add(MessageEvent)
 def handle_something(event):
     if event.message.type=='text':
         recrive_text=event.message.text
         # print(recrive_text)
-        if '器材操作說明' in recrive_text:
+        if '飲食推薦' in recrive_text:
             # print(url_for('static', filename='images/brown_1024.jpg', _external=True))
-            call_service(event)
-        elif '我想知道跑步機怎麼用' in recrive_text:
-            messages=[]
-            messages.append(ImageSendMessage(original_content_url='https://youso.hk/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/g/6/g6481_pioneerdual.jpg', preview_image_url='https://youso.hk/media/catalog/product/cache/1/image/1200x1200/9df78eab33525d08d6e5fb8d27136e95/g/6/g6481_pioneerdual.jpg'))
-            messages.append(TextSendMessage(text='跑步機介紹待定'))
-            messages.append(another_service_or_not)
-            line_bot_api.reply_message(event.reply_token, messages)  
-        elif '我想知道滑步機怎麼用' in recrive_text:
-            messages=[]
-            messages.append(ImageSendMessage(original_content_url='https://i.imgur.com/H8O5GVT.png', preview_image_url='https://i.imgur.com/JM2MHSi.png'))
-            messages.append(TextSendMessage(text='滑步機介紹待定'))
-            messages.append(another_service_or_not)
-            line_bot_api.reply_message(event.reply_token, messages) 
-        elif '我想知道划船機怎麼用' in recrive_text:
-            messages=[]
-            messages.append(ImageSendMessage(original_content_url='https://i.imgur.com/H8O5GVT.png', preview_image_url='https://i.imgur.com/JM2MHSi.png'))
-            messages.append(TextSendMessage(text='划船機機介紹待定'))
-            messages.append(another_service_or_not)
-            line_bot_api.reply_message(event.reply_token, messages) 
-
-        elif '飲食推薦' in recrive_text:
             all_service(event)
+        
+        elif '客房介紹' in recrive_text:
+            messages=[]
+            messages.append(ImageSendMessage(original_content_url='https://i.imgur.com/H8O5GVT.png', preview_image_url='https://i.imgur.com/JM2MHSi.png'))
+            messages.append(TextSendMessage(text='這次入住的是中央飯店的經典客房，客房裝潢以溫暖的大地色系為基調，簡約時尚的設計風格，搭配大片落地玻璃窗，讓自然陽光灑入，住客能在舒適的房間內，遠眺樹海美景及飽覽中壢都會景觀。簡潔俐落的線條經過細節化處理，呈現客房空間設計的時尚氛圍及本真之美。'))
+            messages.append(service_or_not)
+            line_bot_api.reply_message(event.reply_token, messages)  
+
+
         elif '想來點你推薦的餐點' in recrive_text:
             recommend_choice(event)
         elif '想知道一些小建議' in recrive_text:
@@ -217,6 +210,7 @@ def handle_something(event):
             messages.append(TextSendMessage(text='澱粉：五穀米、糙米、藜麥、燕麥片\n蛋白質：優格、牛奶、無糖豆漿、雞蛋、鮭魚、火雞肉等；\n健康脂肪：橄欖油、堅果\n綜合蔬果：高麗菜、空心菜、奇異果、芭樂'))
             messages.append(service_or_not)
             line_bot_api.reply_message(event.reply_token, messages)
+            
             
         elif '想看看有關減脂階段的建議' in recrive_text:
             messages=[]
@@ -339,20 +333,29 @@ def handle_something(event):
             messages.append(service_or_not)
             line_bot_api.reply_message(event.reply_token, messages)
             
-        elif '計算BMI' in recrive_text:
-            call_BMI(event)
+
+            
+
+
+
+
+        elif '美食地圖' in recrive_text:
+            call_food(event)
+        elif '中式餐點小吃' in recrive_text:
+            call_Chinese_food(event)
+        elif '西式餐點小吃' in recrive_text:
+            call_western_food(event)
+        elif '生態導覽' in recrive_text:
+            call_service(event)
+            # messages=[]
+            # messages.append(StickerSendMessage(package_id=446, sticker_id=2000))
+            # messages.append(TextSendMessage(text='中央飯店正努力撰寫程式碼中，請稍後再回來查看此功能~ 謝謝您~'))
+            # line_bot_api.reply_message(event.reply_token, messages)
         else:
-            calculate_BMI(event,recrive_text)
-
-
-
-
-
-
-
-
-
-
+            messages=[]
+            messages.append(StickerSendMessage(package_id=789, sticker_id=10882))
+            messages.append(TextSendMessage(text='抱歉我沒聽懂~ 可以用其他方式再說一次嗎?'))
+            line_bot_api.reply_message(event.reply_token, messages)
     elif event.message.type=='sticker':
         receive_sticker_id=event.message.sticker_id
         receive_package_id=event.message.package_id
@@ -374,26 +377,8 @@ def handle_something(event):
         # print('Transcribe:', text)
         if '服務' in text:
             # print(url_for('static', filename='images/brown_1024.jpg', _external=True))
-            call_service(event)
+            all_service(event)
 
-another_service_or_not = TemplateSendMessage(
-    alt_text='Confirm template',
-    template=ConfirmTemplate(
-        text='請問還需要其他機器的介紹嗎?',
-        actions=[
-            PostbackAction(
-                label='還需要',
-                display_text='還需要',
-                data='action=還需要其他介紹'
-            ),
-            PostbackAction(
-                label='暫時先不用',
-                display_text='暫時先不用',
-                data='action=暫時先不用其他介紹'
-            )
-        ]
-    )
-)
 
 # run app
 if __name__ == "__main__":
--- service_actions/food_bonny.py
+++ service_actions/food_bonny.py
@@ -3,26 +3,27 @@ import datetime
 from line_chatbot_api import *
 
 
+
 def all_service(event):
     message = TemplateSendMessage(
         alt_text='Buttons template',
         template=ButtonsTemplate(
-            # thumbnail_image_url=url_for('static', filename='foodpng/7all.png', _external=True),
-            thumbnail_image_url='https://i.imgur.com/oERO00m.png',
+            # thumbnail_image_url=url_for('static', filename='images/all.png', _external=True),
+            thumbnail_image_url='https://i.imgur.com/VTC5Vbq.png',
             title='請問想體驗哪項服務呢?',
             text='點選一個吧!',
             actions=[
                 MessageAction(
                     label='餐點推薦',
-                    text='想來點推薦的料理'
+                    text='想來點你推薦的餐點'
                 ),
                 MessageAction(
                     label='健身飲食提醒',
-                    text='想知道一些小建議~'
+                    text='想知道一些小建議'
                 ),
                 MessageAction(
                     label='問題與解答',
-                    text='幫我解惑'
+                    text='幫我解惑一下吧'
                 ),
                 MessageAction(
                     label='其他補充',
@@ -35,13 +36,13 @@ def all_service(event):
 
 
 
-def other_choice(event):
+def recommend_choice(event):
     message = TemplateSendMessage(
-        alt_text='想來點推薦的料理',
+        alt_text='Buttons template',
         template=CarouselTemplate(
             columns=[
                 CarouselColumn(
-                    thumbnail_image_url='https://img.onl/ZvZAhx',
+                    thumbnail_image_url='https://i.imgur.com/3pr2gMQ.png',
                     title='正餐推薦',
                     text='想選多少熱量的餐點呢?',
                     actions=[
@@ -50,21 +51,20 @@ def other_choice(event):
                             display_text='想吃0-300大卡的餐點，來點推薦吧!',
                             data='action=想來點推薦的料理&item=0-300'
                         ),
-                         PostbackAction(
+                        PostbackAction(
                             label='300-550',
                             display_text='給我個300-550的餐點吧!',
                             data='action=想來點推薦的料理&item=300-550'
                         ),
-                         PostbackAction(
+                        PostbackAction(
                             label='550-800',
                             display_text='胃口大開，550-800的來一份!',
                             data='action=想來點推薦的料理&item=550-800'
                         )
-
                     ]
                 ),
                 CarouselColumn(
-                    thumbnail_image_url='https://img.onl/ZvZAhx',
+                    thumbnail_image_url='https://i.imgur.com/3pr2gMQ.png',
                     title='早餐推薦',
                     text='想選多少熱量的餐點呢?',
                     actions=[
@@ -73,32 +73,37 @@ def other_choice(event):
                             display_text='想吃0-250大卡的餐點，來點推薦吧!',
                             data='action=想來點推薦的料理&item=0-250'
                         ),
-                         PostbackAction(
+                        PostbackAction(
                             label='250-450',
                             display_text='給我個250-450的餐點吧!',
                             data='action=想來點推薦的料理&item=250-450'
                         ),
-                         PostbackAction(
+                        PostbackAction(
                             label='450-600',
-                            display_text='胃口大開，550-800的來一份!',
+                            display_text='早餐當然要吃飽，推個450-600的食物!',
                             data='action=想來點推薦的料理&item=450-600'
                         )  
                     ]
                 ),
                 CarouselColumn(
-                    thumbnail_image_url='https://img.onl/cFDsIi',
+                    thumbnail_image_url='https://i.imgur.com/jA2pU6S.png',
                     title='隨機推薦',
                     text='想選多少熱量的餐點呢?',
                     actions=[
                         PostbackAction(
                             label='份量大',
-                            display_text='想吃小份量的餐點!',
+                            display_text='想吃大份量的餐點!',
                             data='action=想來點推薦的料理&item=份量大'
                         ),
-                         PostbackAction(
+                        PostbackAction(
                             label='份量小',
                             display_text='想吃小份量的餐點!',
                             data='action=想來點推薦的料理&item=份量小'
+                        ),
+                        PostbackAction(
+                            label='水果推薦',
+                            display_text='水果推薦',
+                            data='action=想來點推薦的料理&item=水果推薦'
                         )
                     ]
                 )
@@ -106,4 +111,246 @@ def other_choice(event):
         )
     )
     line_bot_api.reply_message(event.reply_token, message)
+
+
+
+
+def hint_service(event):
+    message = TemplateSendMessage(
+        alt_text='幫我解惑一下吧',
+        template=CarouselTemplate(
+            columns=[
+                CarouselColumn(
+                    thumbnail_image_url='https://i.imgur.com/u8uB9Jo.png',
+                    title='問題集1',
+                    text='想選多少熱量的餐點呢?',
+                    actions=[
+                        MessageAction(
+                            label='蛋白質吸收率是什麼？',
+                            text='蛋白質吸收率是什麼？'
+                        ),
+                         MessageAction(
+                            label='乳清蛋白真的有效嗎？',
+                            text='乳清蛋白真的有效嗎？'
+                        ),
+                         MessageAction(
+                            label='健身多快能看見成果？',
+                            text='健身多快能看見成果？'
+                        )
+
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url='https://i.imgur.com/u8uB9Jo.png',
+                    title='問題集2',
+                    text='想選多少熱量的餐點呢?',
+                    actions=[
+                        MessageAction(
+                            label='有段時間沒運動，身材會荒廢?',
+                            text='有段時間沒運動，身材會荒廢'
+                        ),
+                         MessageAction(
+                            label='吃點心都會覺得罪惡感高!',
+                            text='吃點心都會覺得罪惡感高!'
+                        ),
+                         MessageAction(
+                            label='吃外食還能維持好身材嗎？',
+                            text='吃外食還能維持好身材嗎'
+                        )  
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url='https://i.imgur.com/O7Qh0jv.png',
+                    title='問題集3-數值介紹',
+                    text='想選多少熱量的餐點呢?',
+                    actions=[
+                        MessageAction(
+                            label='BMR-基礎代謝率',
+                            text='基礎代謝率是什麼'
+                        ),
+                        MessageAction(
+                            label='TDEE-每日總消耗熱量',
+                            text='每日總消耗熱量是什麼'
+                        ),
+                        MessageAction(
+                            label='改變的方法!',
+                            text='改變代謝力'
+                        )  
+                    ]
+                )
+            ]
+        )
+    )
+    line_bot_api.reply_message(event.reply_token, message)
+
+
+def hint_service(event):
+    message = TemplateSendMessage(
+        alt_text='想知道一些小建議~',
+        template=ButtonsTemplate(
+            # thumbnail_image_url=url_for('static', filename='images/hint.png', _external=True),
+            thumbnail_image_url='https://i.imgur.com/PfSTVkL.png',
+            title='請問想知道哪項建議呢?',
+            text='點選一個吧!',
+            actions=[
+                MessageAction(
+                    label='增肌階段',
+                    text='想知道有關增肌階段的建議'
+                ),
+                MessageAction(
+                    label='減脂階段',
+                    text='想看看有關減脂階段的建議'
+                ),
+                MessageAction(
+                    label='休息階段',
+                    text='來點休息期的建議吧'
+                ),
+                MessageAction(
+                    label='其他建議',
+                    text='看看其他建議有哪些'
+                )
+            ]
+        )
+    )
+    line_bot_api.reply_message(event.reply_token, message)
+
+
+def answer_service(event):
+    message = TemplateSendMessage(
+        alt_text='幫我解惑一下吧',
+        template=CarouselTemplate(
+            columns=[
+                CarouselColumn(
+                    thumbnail_image_url='https://i.imgur.com/u8uB9Jo.png',
+                    title='問題集1',
+                    text='想選多少熱量的餐點呢?',
+                    actions=[
+                        MessageAction(
+                            label='蛋白質吸收率是什麼？',
+                            text='蛋白質吸收率是什麼？'
+                        ),
+                         MessageAction(
+                            label='乳清蛋白真的有效嗎？',
+                            text='乳清蛋白真的有效嗎？'
+                        ),
+                         MessageAction(
+                            label='健身多快能看見成果？',
+                            text='健身多快能看見成果？'
+                        )
+
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url='https://i.imgur.com/u8uB9Jo.png',
+                    title='問題集2',
+                    text='想選多少熱量的餐點呢?',
+                    actions=[
+                        MessageAction(
+                            label='有段時間沒運動，身材會荒廢?',
+                            text='有段時間沒運動，身材會荒廢'
+                        ),
+                         MessageAction(
+                            label='吃點心都會覺得罪惡感高!',
+                            text='吃點心都會覺得罪惡感高!'
+                        ),
+                         MessageAction(
+                            label='吃外食還能維持好身材嗎？',
+                            text='吃外食還能維持好身材嗎'
+                        )  
+                    ]
+                ),
+                CarouselColumn(
+                    thumbnail_image_url='https://i.imgur.com/O7Qh0jv.png',
+                    title='問題集3-數值介紹',
+                    text='想選多少熱量的餐點呢?',
+                    actions=[
+                        MessageAction(
+                            label='BMR-基礎代謝率',
+                            text='基礎代謝率是什麼'
+                        ),
+                        MessageAction(
+                            label='TDEE-每日總消耗熱量',
+                            text='每日總消耗熱量是什麼'
+                        ),
+                        MessageAction(
+                            label='改變的方法!',
+                            text='改變代謝力'
+                        )  
+                    ]
+                )
+            ]
+        )
+    )
+    line_bot_api.reply_message(event.reply_token, message)
+
+def other_service(event):
+    message = TemplateSendMessage(
+        alt_text='想看看其他說明',
+        template=ButtonsTemplate(
+            # thumbnail_image_url=url_for('static', filename='foodpng/other.png', _external=True),
+            thumbnail_image_url='https://i.imgur.com/1TuMdxc.png',
+            title='請問想看看哪項說明?',
+            text='點選一個吧!',
+            actions=[
+                MessageAction(
+                    label='自助餐',
+                    text='跟我說有關挑選自助餐的事吧'
+                ),
+                MessageAction(
+                    label='大餐',
+                    text='想吃想吃，有甚麼該注意的'
+                ),
+                MessageAction(
+                    label='水果',
+                    text='有關水果，我該知道甚麼呢'
+                ),
+                    MessageAction(
+                    label='飲料',
+                    text=' 跟我說說選飲料的大學問!'
+                )
+            ]
+        )
+    )
+    line_bot_api.reply_message(event.reply_token, message)
+
+
+service_or_not= TemplateSendMessage(
+        alt_text='Confirm template',
+        template=ConfirmTemplate(
+            text='請問還需要其他服務嗎?',
+            actions=[
+                PostbackAction(
+                    label='還需要服務',
+                    display_text='還需要你的幫忙',
+                    data='action=還需要'
+                ),
+                PostbackAction(
+                    label='暫時先不用',
+                    display_text='這樣就好，謝謝!',
+                    data='action=暫時先不用'
+                )
+            ]
+        )
+    )
+
+another_or_not= TemplateSendMessage(
+        alt_text='Confirm template',
+        template=ConfirmTemplate(
+            text='要重選一次嗎?',
+            actions=[
+                PostbackAction(
+                    label='再來一次吧!',
+                    display_text='再給我一個當參考吧!',
+                    data='action=再來一個吧'
+                ),
+                PostbackAction(
+                    label='這個很好!',
+                    display_text='這樣就好，謝謝!',
+                    data='action=暫時先不用'
+                )
+            ]
+        )
+    )
+
+
 

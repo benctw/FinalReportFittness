6ca6b81ae6378023889c5be0b5eb7f79aa867bf8
idler <s0973913299@gmailcom>
2021-12-18 14:47:23 +0800

    UD

--- service_actions/introduction.py
+++ service_actions/introduction.py
@@ -26,11 +26,7 @@ def call_introduction(event):
                 MessageAction(
                     label='多功能單/雙槓輔助機',
                     text='我想知道多功能單/雙槓輔助機怎麼用'
-                ),
-                MessageAction(
-                    label='腿內收機',
-                    text='我想知道腿內收機怎麼用'
-                ),
+                ),,
             ]
         )
     )

7f428589a5d78f3268d6716f822464310bc7b762
idler <s0973913299@gmailcom>
2021-12-18 16:51:58 +0800

    UD

--- service_actions/introduction.py
+++ service_actions/introduction.py
@@ -29,5 +29,32 @@ def call_introduction(event):
                 )
             ]
         )
+    ),
+    message = TemplateSendMessage(
+        alt_text='Buttons template',
+        template=ButtonsTemplate(
+            # thumbnail_image_url=url_for('static', filename='images/brown_1024.jpg', _external=True),
+            thumbnail_image_url='https://i.imgur.com/rfgMcFM.jpg',
+            title='請問想要知道什麼設施的介紹呢?',
+            text='請在下方點選您想知道的項目',
+            actions=[
+                MessageAction(
+                    label='腹部前驅訓練機',
+                    text='我想知道腹部前驅訓練機怎麼用'
+                ),
+                MessageAction(
+                    label='二頭訓練機',
+                    text='我想知道二頭訓練機怎麼用'
+                ),
+                MessageAction(
+                    label='腿部伸張機',
+                    text='我想知道腿部伸張機怎麼用'
+                ),
+                MessageAction(
+                    label='腿內收機',
+                    text='我想知道腿內收機怎麼用'
+                )
+            ]
+        )
     )
     line_bot_api.reply_message(event.reply_token, message)

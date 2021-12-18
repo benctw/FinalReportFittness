8a30db9f533e3b6657d399946be5cfa223844b68
idler <s0973913299@gmailcom>
2021-12-18 17:59:30 +0800

    UD

--- service_actions/introduction.py
+++ service_actions/introduction.py
@@ -13,15 +13,15 @@ def call_introduction(event):
                     title='請問想要知道什麼設施的介紹呢?',
                     text='請在下方點選您想知道的項目',
                     actions=[
-                        PostbackAction(
+                        MessageAction(
                         label='臥姿彎腿機',
                         display_text='我想知道臥姿彎腿機怎麼用'
                         ),
-                        PostbackAction(
+                        MessageAction(
                         label='背伸機',
                         display_text='我想知道背伸機怎麼用'
                         ),
-                        PostbackAction(
+                        MessageAction(
                         label='大腿推蹬機',
                         display_text='我想知道大腿推蹬機怎麼用'
                         )
@@ -32,15 +32,15 @@ def call_introduction(event):
                     title='請問想要知道什麼設施的介紹呢?',
                     text='請在下方點選您想知道的項目',
                     actions=[
-                        PostbackAction(
+                        MessageAction(
                         label='多功能單/雙槓輔助機',
                         display_text='我想知道多功能單/雙槓輔助機怎麼用'
                         ),
-                        PostbackAction(
+                        MessageAction(
                         label='腹部前驅訓練機',
                         display_text='我想知道腹部前驅訓練機怎麼用'
                         ),
-                        PostbackAction(
+                        MessageAction(
                         label='二頭訓練機',
                         display_text='我想知道二頭訓練機怎麼用'
                         )
@@ -51,15 +51,15 @@ def call_introduction(event):
                     title='請問想要知道什麼設施的介紹呢?',
                     text='請在下方點選您想知道的項目',
                     actions=[
-                        PostbackAction(
+                        MessageAction(
                         label='腿部伸張機',
                         display_text='我想知道腿部伸張機怎麼用'
                         ),
-                        PostbackAction(
+                        MessageAction(
                         label='腿內收機',
                         display_text='我想知道腿內收機怎麼用'
                         ),
-                        PostbackAction(
+                        MessageAction(
                         label='腿內收機',
                         display_text='我想知道腿內收機怎麼用'
                         )

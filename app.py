c5aa827afd24bb00a240beb928ad3ade0416d0e4
Benjamin <benctw@gmail.com>
2022-01-09 00:01:20 +0800

    把port改為5002

--- app.py
+++ app.py
@@ -717,4 +717,4 @@ another_service_or_not = TemplateSendMessage(
 
 # run app
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5566, debug=True)
+    app.run(host='127.0.0.1', port=5002, debug=True)
